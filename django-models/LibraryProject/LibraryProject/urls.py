fbae9d2c5dfe1639963f4c57b1506c9d1da60494
AK21ER <itsmereka21@gmail.com>
2025-07-20 13:46:00 -0700

    Update urls.py

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('relationship/', include('relationship_app.urls')),
+  path('admin/', admin.site.urls),
+    path('auth/', include('relationship_app.urls')),  # Authentication URLs
 ]
 
