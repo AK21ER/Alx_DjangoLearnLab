29c36942b88650d72fa94e236449cd8879acad22
AK21ER <itsmereka21@gmail.com>
2025-07-21 00:02:04 -0700

    ooo

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
-from .models import Book, Library
+from django.contrib.auth.decorators import login_required, user_passes_test
+from .models import UserProfile
 
-# Function-based view to list all books
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+def check_role(role):
+    def inner(user):
+        return hasattr(user, 'userprofile') and user.userprofile.role == role
+    return inner
 
-# Class-based view to show library details
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
+@login_required
+@user_passes_test(check_role('Admin'))
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@login_required
+@user_passes_test(check_role('Librarian'))
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@login_required
+@user_passes_test(check_role('Member'))
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
 
