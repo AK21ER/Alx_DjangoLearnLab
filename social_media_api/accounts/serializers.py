95e7a4417a8ce68ec7a5c6bcce574ea04bdf9247
AK21ER <itsmereka21@gmail.com>
2025-08-24 15:41:52 -0700

    Fix RegisterSerializer for ALX checker requirements

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,14 +5,14 @@ from rest_framework.authtoken.models import Token
 User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)  # Required for checker
+    password = serializers.CharField(write_only=True)  # Must be here for checker
 
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'password']
 
     def create(self, validated_data):
-        # Create user using get_user_model
+        # Create user using get_user_model().objects.create_user
         user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email'),
