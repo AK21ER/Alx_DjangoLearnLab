8cdf47f34496235cfd78f4f0d5d3e1f78a89789f
AK21ER <itsmereka21@gmail.com>
2025-08-24 20:33:32 -0700

    Add user follow system and feed endpoint

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,15 +1,12 @@
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet ,
-from .views import feed
-from django.urls import path
+from django.urls import path, include
+from .views import PostViewSet, CommentViewSet, feed
+
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
 router.register(r'comments', CommentViewSet, basename='comment')
-router.register(r'posts', PostViewSet, basename='post')
-router.register(r'comments', CommentViewSet, basename='comment')
 
-
-urlpatterns = router.urls + [
-    path('feed/', feed, name='feed')
+urlpatterns = [
+    path('feed/', feed, name='feed'),
+    path('', include(router.urls)),
 ]
-
